715e668de81fa3b010da800902056b31cd6e40de
mehrinmaisha <merhin.maisha2003@gmail.com>
2020-07-23 18:55:56 +0000

    coding course

--- newfile.py
+++ newfile.py
@@ -1,1 +1,35 @@
-print"Hello World"
+def add(x,y):
+    return int(x) + int(y)
+def subtract(x,y):
+    return int(x) - int(y)
+def multiply(x,y):
+    return int(x) * int(y)
+def divide(x,y):
+    return int(x) / int(y)
+
+
+print("1,Add")
+print("2,subtract")
+print("3,Multiply")
+print("4,Divide")
+
+
+choice = input("select operation:")
+
+if choice in ('1', '2', '3', '4'):
+    num1 = float(input("Enter first number:"))
+    num2 = float(input("Enter second number:"))
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1,num2))
+    elif  choice == "2":
+        print(num1, "-", num2, "=", subtract(num1,num2))
+
+    elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1,num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1,num2))
+    
+else:
+    print("Invalid input")
+
